f5905c1a8b8fc81ba6c58330f32fbeb80f32b67b
jfbs <jeffmbowers@gmail.com>
2013-04-16 22:21:39 -0400

    Update cm scripts

--- reset.sh
+++ reset.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
 cd ~/android/system/hardware/qcom/audio
-git merge --no-commit --no-ff audio-patch
+git merge --no-commit --no-ff jelly
+
+cd ~/android/system/system/core
+git merge --no-commit --no-ff jelly
+
+cd ~/android/system/device/common
+git merge --no-commit --no-ff jelly
+
+cd ~/android/system/external/wpa_supplicant_8
+git merge --no-commit --no-ff jelly
+
+cd ~/android/system/hardware/qcom/display
+git merge --no-commit --no-ff jelly
 
 cd ~/android/system/frameworks/base
-git merge --no-commit --no-ff phone-patch
+git merge --no-commit --no-ff jelly
 
 cd ~/android/system/frameworks/av
-git merge --no-commit --no-ff pix-patch
+git merge --no-commit --no-ff jelly
 
 cd ~/android/system/frameworks/native
-git merge --no-commit --no-ff t-foto
-
-# cd ~/android/system/frameworks/native
-# git merge --no-commit --no-ff foto-patch
+git merge --no-commit --no-ff jelly
 
 echo "done"
--- unset.sh
+++ unset.sh
@@ -2,23 +2,38 @@
 
 cd ~/android/system/hardware/qcom/audio
 git merge --abort
-echo "Unmerged audio-patch.\n"
+echo "Unmerged audio patch.\n"
+
+cd ~/android/system/system/core
+git merge --abort
+echo "Unmerged core power-patch.\n"
+
+cd ~/android/system/device/common
+git merge --abort
+echo "Unmerged gps patch.\n"
+
+cd ~/android/system/external/wpa_supplicant_8
+git merge --abort
+echo "Unmerged wep fix.\n"
+
+cd ~/android/system/hardware/qcom/display
+git merge --abort
+echo "Unmerged display patch.\n"
 
 cd ~/android/system/frameworks/base
 git merge --abort
-echo "Unmerged phone-patch.\n"
+echo "Unmerged phone patch.\n"
 
 cd ~/android/system/frameworks/av
 git merge --abort
-echo "Unmerged pix-patch.\n"
+echo "Unmerged av patch.\n"
 
 cd ~/android/system/frameworks/native
 git merge --abort
-echo "Unmerged t-patch.\n"
+echo "Unmerged native patch.\n"
 
-# cd ~/android/system/frameworks/native
-# git merge --abort
-# echo "Unmerged foto-patch.\n"
+# cd ~/android/system/prebuilts
+# git merge --no-commit --no-ff jellybean
 
 echo "done"
 
