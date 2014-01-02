dac936f9046cf64b765ab1db60ab2561a94a5060
jfbs <jeffmbowers@gmail.com>
2014-01-01 19:47:20 -0500

    update branches

--- set440.sh
+++ set440.sh
@@ -12,7 +12,7 @@ else
 	exit 0
 fi
 
-declare -a arr=(build bionic frameworks/av frameworks/native hardware/qcom/audio-caf hardware/qcom/media-caf packages/apps/Settings vendor/cm)
+declare -a arr=(build dalvik frameworks/av frameworks/base frameworks/native hardware/qcom/audio-caf hardware/qcom/display-caf hardware/qcom/media-caf packages/apps/Settings system/vold vendor/cm)
 
 for i in ${arr[@]}; do
 	cd $HOME/android/system/$i
@@ -22,7 +22,7 @@ done
 echo Done.
 exit 0
 
-#  hardware/libhardware_legacy packages/apps/Gallery2 packages/apps/Dialer system/vold frameworks/base packages/apps/Camera2 system/core frameworks/av hardware/qcom/display-caf
-#  packages/services/Telephony packages/providers/TelephonyProvider packages/apps/Mms
-# hardware/qcom/media  hardware/qcom/wlan
-# packages/apps/Torch
+# hardware/libhardware_legacy packages/apps/Gallery2 packages/apps/Dialer system/vold system/core 
+# packages/services/Telephony packages/providers/TelephonyProvider packages/apps/Mms packages/apps/Settings 
+# hardware/qcom/media  hardware/qcom/wlan packages/apps/Torch
+# packages/apps/Camera2 bionic
