8fdb3b049c9b25fcbc0e11e0f562d9e23037e9ab
jfbs <jeffmbowers@gmail.com>
2014-02-21 20:49:30 -0500

    update repos

--- set440.sh
+++ set440.sh
@@ -12,7 +12,7 @@ else
 	exit 0
 fi
 
-declare -a arr=(build development frameworks/av frameworks/base frameworks/native frameworks/rs frameworks/webview 
+declare -a arr=(build development frameworks/base frameworks/native frameworks/rs frameworks/webview 
                 hardware/qcom/audio-caf hardware/qcom/display-caf packages/apps/Settings system/vold vendor/cm
                 external/openssl external/clang packages/services/Telephony packages/inputmethods/LatinIME)
 
@@ -27,5 +27,5 @@ exit 0
 # hardware/libhardware_legacy packages/apps/Gallery2 system/core 
 # packages/providers/TelephonyProvider packages/apps/Mms 
 # hardware/qcom/media  hardware/qcom/wlan hardware/qcom/media-caf packages/apps/Torch
-# packages/apps/Camera2 bionic dalvik external/chromium_org
+# packages/apps/Camera2 bionic dalvik external/chromium_org frameworks/av
 # packages/apps/Contacts packages/apps/Dialer packages/apps/InCallUI packages/apps/ContactsCommon packages/providers/MediaProvider 
